558dc6a68da7e133b7ad177c9a2ff5f45047532c
santhitak <santhita.sk@gmail.com>
2022-07-22 16:59:51 +0700

    :bug: : query

--- islands/Lotto.tsx
+++ islands/Lotto.tsx
@@ -3,6 +3,7 @@ import { h } from "preact";
 import { tw } from "@twind";
 import { useEffect, useState } from "preact/hooks";
 import { LottoReq } from "../constant/lotto.d.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import {
   adjustCenter,
   btn,
@@ -11,9 +12,13 @@ import {
   inputBar,
 } from "../utils/style.tsx";
 
-export default function Lotto() {
-  const [lotto, setLotto] = useState<LottoReq>();
+interface Data {
+  results?: LottoReq["response"]["prizes"];
+  query: string;
+}
 
+export default function Lotto({ data }: PageProps<Data>) {
+  const [lotto, setLotto] = useState<LottoReq>();
   const getLotto = async () => {
     const res = await fetch("/api/lottoyo");
     const resp: LottoReq = await res.json();
@@ -24,8 +29,26 @@ export default function Lotto() {
     getLotto();
   });
 
+  const handler: Handlers<Data> = {
+    GET(req, ctx) {
+      const url = new URL(req.url);
+      const query = url.searchParams.get("searchLotto") || "";
+      const results = lotto?.response.prizes.filter((lucky) =>
+        lucky.number.includes(query)
+      );
+      return ctx.render({ results, query });
+    },
+  };
+
+  const { results, query } = data;
+
   return (
     <div>
+      <div>
+        {results.map((prize) => (
+          <li key={prize}>{prize}</li>
+        ))}
+      </div>
       <div class={tw`mx-auto max-w-screen-md my-12`}>
         <p class={tw`text-6xl font-bold text-center`}>ผลสลากกินแบ่ง</p>
         <p class={tw`text-4xl font-bold text-center mt-4`}>
@@ -38,10 +61,13 @@ export default function Lotto() {
               class={tw`${inputBar}`}
               placeholder="ค้นหาสลากกินแบ่งของท่าน"
               name="searchLotto"
-              disabled
+              value={query}
             />
             <div class={tw`absolute top-2 right-2`}>
-              <button class={tw`${btn} h-10 w-20 p-0 font-normal m-0`} disabled>
+              <button
+                class={tw`${btn} h-10 w-20 p-0 font-normal m-0`}
+                type="submit"
+              >
                 Search
               </button>
             </div>
